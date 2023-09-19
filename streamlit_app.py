12f36c1df972b2678ef3df7aea5c628cb1b4533f
ltor-lpnc <laurent.torlay@univ-grenoble-alpes.fr>
2023-09-19 10:49:09 +0200

    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.title("Prediction of status")
 st.markdown("We use Synt and Phon scores to predict status")
 
 st.subheader("Enter Synt Score : ")
-synt = st.number_input('', 0,1)
+synt = st.number_input('', 0,1,key='synt')
 
 st.subheader("Enter Phon Score : ")
-phon = st.number_input('', 0,1)
+phon = st.number_input('', 0,1,key='phon')
 
 st.subheader("Status prediction :")
 X = np.array([synt,phon]).reshape(1, -1)
